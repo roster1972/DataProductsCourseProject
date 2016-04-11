cf257d9ae60352fa3f5d3a922706eedddac059e4
Robert Osterried <roster1972@hotmail.com>
2016-04-11 17:54:26 -0400

    Initial commit for source files.

---
+++ ui.R
@@ -0,0 +1,13 @@
+
+
+library(shiny)
+shinyUI(pageWithSidebar(
+    headerPanel("Data science FTW!"),
+    sidebarPanel(
+        h3('Sidebar text')
+    ),
+    mainPanel(
+        h3('Main Panel text')
+    )
+))
+
