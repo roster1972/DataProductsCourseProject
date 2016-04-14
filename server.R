63dfc2335a8d6ab51949591e73a530a256c18736
Robert Osterried <roster1972@hotmail.com>
2016-04-14 18:26:32 -0400

    Another commit.

--- server.R
+++ server.R
@@ -1,5 +1,19 @@
 library(shiny)
+
+diabetesRisk <- function(glucose) glucose / 200
+
 shinyServer(
     function(input, output) {
+        output$outputmiles <- renderPrint({input$inputkm * 0.6214})
+        output$outputyards <- renderPrint({input$inputm * 1.0936})
+        output$outputpounds <- renderPrint({input$inputkg * 2.2046})
+        output$outputounces <- renderPrint({input$inputgram * 0.0353})
+        output$outputpints <- renderPrint({input$inputliter * 2.1133})
+        
+        output$outputkm <- renderPrint({input$inputmiles * 1.609})
+        output$outputm <- renderPrint({input$inputyards * 0.914})
+        output$outputkg <- renderPrint({input$inputpounds * 0.453})
+        output$outputgram <- renderPrint({input$inputounces * 28.350})
+        output$outputliter <- renderPrint({input$inputpints * 0.473176})
     }
 )
--- ui.R
+++ ui.R
@@ -1,13 +1,57 @@
 
 
 library(shiny)
-shinyUI(pageWithSidebar(
-    headerPanel("Data science FTW!"),
-    sidebarPanel(
-        h3('Sidebar text')
-    ),
+shinyUI(fluidPage(
+    headerPanel("British/Metric Unit Conversion"),
     mainPanel(
-        h3('Main Panel text')
+        fluidRow(
+            column(6,
+                   h3('Metric'),
+                   numericInput('inputkm', 'Kilometers', 0, min = 0, max = 1000, step = 10),
+                   numericInput('inputm', 'Meters', 0, min = 0, max = 1000, step = 10),
+                   numericInput('inputkg', 'Kilograms', 0, min = 0, max = 1000, step = 10),
+                   numericInput('inputgram', 'Grams', 0, min = 0, max = 1000, step = 10),
+                   numericInput('inputliter', 'Liters', 0, min = 0, max = 1000, step = 10)
+            ),
+            column(6,
+                   h3('British'),
+                   h4('Miles:'),
+                   verbatimTextOutput("outputmiles"),
+                   h4('Yards:'),
+                   verbatimTextOutput("outputyards"),
+                   h4('Pounds:'),
+                   verbatimTextOutput("outputpounds"),
+                   h4('Ounces:'),
+                   verbatimTextOutput("outputounces"),
+                   h4('Pints:'),
+                   verbatimTextOutput("outputpints")
+            )
+        ),
+        fluidRow(
+            column(6,
+                   h3('British'),
+                   numericInput('inputmiles', 'Miles', 0, min = 0, max = 1000, step = 10),
+                   numericInput('inputyards', 'Yards', 0, min = 0, max = 1000, step = 10),
+                   numericInput('inputpounds', 'Pounds', 0, min = 0, max = 1000, step = 10),
+                   numericInput('inputounces', 'Ounces', 0, min = 0, max = 1000, step = 10),
+                   numericInput('inputpints', 'Pints', 0, min = 0, max = 1000, step = 10)
+            ),
+            column(6,
+                   h3('Metric'),
+                   h4('Kilometers:'),
+                   verbatimTextOutput("outputkm"),
+                   h4('Meters:'),
+                   verbatimTextOutput("outputm"),
+                   h4('Kilograms:'),
+                   verbatimTextOutput("outputkg"),
+                   h4('Grams:'),
+                   verbatimTextOutput("outputgram"),
+                   h4('Liters:'),
+                   verbatimTextOutput("outputliter"),   
+                   
+                   submitButton('Submit')
+            )
+        )
     )
 ))
 
